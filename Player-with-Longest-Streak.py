7267637b321a9a35187578f677ffadc3d9eec1d1
ygsjason <99283954+ygsjason@users.noreply.github.com>
2022-03-29 16:16:27 -0500

    Update Player-with-Longest-Streak.py

--- Player-with-Longest-Streak.py
+++ Player-with-Longest-Streak.py
@@ -24,6 +24,18 @@ df2['n'] = df2.streak.apply(lambda x: len(x))
 
 df2[df2.n == df2.n.max()]
 
+#Method 3
+m = df['match_result'].str.upper().ne('W')
+t = m.cumsum()
+r = m.cumsum()[~m]
+s = m.cumsum()[~m].groupby(df['player_id']).value_counts().reset_index(level =1, drop = True)
+
+df3 = s.reset_index()
+
+df3[df3.match_result == df3.match_result.max()]
+
+df4 = s.agg({'player_id': 'idxmax', 'longest_winningstreak':'max'}).to_frame(0).T
+
 #df1['new'] = df1.match_result.apply(lambda x: x.replace('', ',')[1:-1])
 #str = 'wwllwwllw'
 #str[1:-1]
